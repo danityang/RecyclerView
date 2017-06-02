38aea24b70425ca1b9d27193d451e0940175fb96
ANDROID-PC-01\yangdi <yangdi128@163.com>
2017-06-02 17:53:16 +0800

    1.增加自定义分割线样式 2.增加注释

--- src/main/java/com/cdemo/recyclerview/MainActivity.java
+++ src/main/java/com/cdemo/recyclerview/MainActivity.java
@@ -3,23 +3,26 @@ package com.cdemo.recyclerview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.OrientationHelper;
 import android.support.v7.widget.RecyclerView;
+import android.view.Window;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    // recyclerview
     RecyclerView recyclerView;
     List<String> list;
+    // 适配器
     private  RecycleAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         initView();
@@ -31,11 +34,21 @@ public class MainActivity extends AppCompatActivity {
         initListData(list);
         adapter = new RecycleAdapter(list, MainActivity.this);
 
+        /*RecyclerView.LayoutManager是一个抽象类，系统为我们提供了三个实现类
+        1.LinearLayoutManager即线性布局，这个是在上面的例子中我们用到的布局
+        2.GridLayoutManager即表格布局
+        3.StaggeredGridLayoutManager即流式布局，如瀑布流效果*/
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(OrientationHelper.HORIZONTAL);
+        linearLayoutManager.setOrientation(OrientationHelper.VERTICAL);
+
+
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this,0));
+        // 添加分割线，后面的参数表示是横线还是竖线，0，横线 1，竖线
+//        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this,0));
+        recyclerView.addItemDecoration(new ItemDecoration(MainActivity.this, LinearLayoutManager.VERTICAL));
+
+        // iteM动画
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
     }
